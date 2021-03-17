6d1528c9de3f27394b2750fe3469ee14ce8875a6
Will Studabaker <will.studabaker@gmail.com>
2021-03-17 13:20:48 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div>
+            <h3>{movie.title}, {movie.time} minutes</h3>
+            <ul>
+              {movie.genres.map(genre =>
+                <li>
+                  {genre}
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
