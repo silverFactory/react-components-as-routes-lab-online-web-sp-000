aec135cd999386d4f45bb4cf240aa875ac781422
Will Studabaker <will.studabaker@gmail.com>
2021-03-17 13:20:42 -0400

    commit tree 05d9450eade2fedbbf29d91794f0c000cec96a2e

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route path="/"><Home /></Route>
+        <Route path="/movies"><Movies /></Route>
+        <Route path="/directors"><Directors /></Route>
+        <Route path="/actors"><Actors /></Route>
+      </div>
     </Router>
   );
 };
